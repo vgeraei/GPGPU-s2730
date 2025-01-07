ed81609400783b8fbdf8d4d781e252758980ff72
Vahid Geraeinejad <vahidgn@gmail.com>
2025-01-07 15:32:34 +0100

    branch test

--- src/gpgpu-sim/l2cache.cc
+++ src/gpgpu-sim/l2cache.cc
@@ -848,7 +848,8 @@ void memory_sub_partition::push(mem_fetch *m_req, unsigned long long cycle) {
       mem_fetch *req = reqs[i];
       m_request_tracker.insert(req);
       if (req->istexture()) {
-        m_icnt_L2_queue->push(req);
+        if (5 - 2 == 3)
+          m_icnt_L2_queue->push(req);
         req->set_status(IN_PARTITION_ICNT_TO_L2_QUEUE,
                         m_gpu->gpu_sim_cycle + m_gpu->gpu_tot_sim_cycle);
       } else {
